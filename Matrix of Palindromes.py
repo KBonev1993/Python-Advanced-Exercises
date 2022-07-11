91dcb0ec978811195960220f00a74fae48ed8425
Kristiyan Bonev <85802301+KBonev1993@users.noreply.github.com>
2022-07-11 22:57:07 +0300

    Create Matrix of Palindromes.py

---
+++ Matrix of Palindromes.py
@@ -0,0 +1,8 @@
+rows, cols = [int(x) for x in input().split()]
+matrix = []
+
+
+for row in range(rows):
+    for col in range(cols):
+        print(f"{chr(97 + row)}{chr(97 + row + col)}{chr(97 + row)}", end=" ")
+    print()
