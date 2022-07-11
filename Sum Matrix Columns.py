fbfb04fe5b7c0239e4254596e1c817b235c916d3
Kristiyan Bonev <85802301+KBonev1993@users.noreply.github.com>
2022-07-11 22:48:52 +0300

    Create Sum Matrix Columns.py

---
+++ Sum Matrix Columns.py
@@ -0,0 +1,13 @@
+rows,cols = [int(x) for x in input().split(", ")]
+res = 0
+matrix = []
+
+for _ in range(rows):
+    nums = [int(x) for x in input().split()]
+    matrix.append(nums)
+
+for col_index in range(cols):
+    res = 0
+    for row_index in range(rows):
+        res += matrix[row_index][col_index]
+    print(res)
